9c43214664b2b8f1c1b6fde8ff901ccbfe9f1e53
JuliansRecent <juliansrecent@gmail.com>
2023-02-02 17:23:15 -0800

    Module complete. Create a terminal based application that prompt the user for their age and using the OR operator says 'OK' or 'Impossible!' depending on if they are in the possible age ranges.

--- src/main/java/CheckingTheAge.java
+++ src/main/java/CheckingTheAge.java
@@ -6,5 +6,14 @@ public class CheckingTheAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Prompt the user for their age
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scan.nextLine());
+        // If they are less than 0 y/o or more than 120 y/o print "Impossible!" to the console
+        if (age < 0 || age > 120) {
+            System.out.println("Impossible!");
+        } else {
+            System.out.println("OK");
+        }
     }
 }
